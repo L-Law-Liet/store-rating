9640409af3e7e818323ebd6c7b63c580a15da91e
L-Law-Liet <a.kadirov.17.06@gmail.com>
2021-11-18 18:03:42 +0500

    Secure Service-to-Service Requests

--- src/main/java/com/example/storeratingservice/service/RatingService.java
+++ src/main/java/com/example/storeratingservice/service/RatingService.java
@@ -5,7 +5,11 @@ import com.example.storeratingservice.entity.Rating;
 import com.example.storeratingservice.repository.RatingRepository;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -32,7 +36,16 @@ public class RatingService {
                     @HystrixProperty(name="maxQueueSize", value="20"),
             })
     public Product getProductById(Long id) {
-        return restTemplate.getForObject("http://store-information-service/products/" + id, Product.class);
+        String apiCredentials = "rest-client:p@ssword";
+        String base64Credentials = new String(Base64.encodeBase64(apiCredentials.getBytes()));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Credentials);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+//        return restTemplate.getForObject("http://store-information-service/products/" + id, Product.class);
+        return restTemplate.exchange("http://store-information-service/products/" + id,
+                HttpMethod.GET, entity, Product.class).getBody();
     }
 
     public Product getProductByIdFallback(Long id) {
